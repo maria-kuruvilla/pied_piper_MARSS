65b7941e748c870ff48e1e532ba2c5a3b243058b
maria-kuruvilla <maria@75799@gmail.com>
2023-05-04 15:38:53 -0700

    removed 0s and logged data

--- MARSS_trial.R
+++ MARSS_trial.R
@@ -3,6 +3,7 @@ library(here)
 library(MARSS)
 library(tidyverse)
 library(ggplot2)
+library(zoo)
 
 
 #readng the data
@@ -32,3 +33,62 @@ fit <- MARSS(subset, model = mod.list, method = "BFGS")
 plot(fit, plot.type = "model.resids.ytt1") # not good - try log
 plot(fit, plot.type = "qqplot.std.model.resids.ytt1") # not good 
 plot(fit, plot.type = "acf.std.model.resids.ytt1") # not good
+
+
+#filtering on doy and removing zeros and logging the data
+subset2 <- arrange(data,doy) %>%
+  filter(year >= 2005 & year < 2010 & doy > 100 & doy <=200) %>%
+  select(coho1_wild_num,year,doy) %>%
+  pivot_wider(names_from = year, values_from = coho1_wild_num) %>%
+  column_to_rownames(var = "doy") %>%
+  as.matrix() %>%
+  t()
+
+
+subset3 <- arrange(data,doy) %>%
+  filter(year >= 2005 & year < 2010 & doy > 100 & doy <=200) %>%
+  mutate(log.value = ifelse(coho1_wild_num == 0, NA, log(coho1_wild_num))) %>%
+  select(log.value,year,doy) %>%
+  pivot_wider(names_from = year, values_from = log.value) %>%
+  column_to_rownames(var = "doy") %>%
+  as.matrix() %>%
+  t()
+
+
+
+mod.list <- list(
+  U = "unequal",
+  R = "diagonal and equal",
+  Q = "diagonal and unequal"
+)
+
+fit3 <- MARSS(subset3, model = mod.list, method = "BFGS")
+
+autoplot(fit3)
+
+
+#adding covariates
+#interplating temp values - need to change
+
+
+subset_covariates <- arrange(data,doy) %>%
+  filter(year >= 2005 & year < 2010 & doy > 100 & doy <=200) %>%
+  mutate(temp =na.approx(temp, na.rm = FALSE, maxgap = 5)) %>%
+  select(temp,year,doy) %>%
+  pivot_wider(names_from = year, values_from = temp) %>%
+  column_to_rownames(var = "doy") %>%
+  as.matrix() %>%
+  t()
+
+mod.list.cov <- list(
+  U = "unequal",
+  R = "diagonal and equal",
+  Q = "diagonal and unequal",
+  C = "diagonal and unequal",
+  c = subset_covariates
+)
+
+
+fit_covariates <- MARSS(subset3, model = mod.list.cov, method = "BFGS")
+
+autoplot(fit_covariates)
