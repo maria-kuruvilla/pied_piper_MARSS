16976977867d56d1e7d82a55cebad4e772f92177
maria-kuruvilla <maria@75799@gmail.com>
2023-05-24 16:55:57 -0700

    tried dlm, coho for shorter period

--- chinook0.R
+++ chinook0.R
@@ -242,3 +242,246 @@ mod.list2 <- mod.list.photo_flow_all_years <- list(
 
 
 fit_chinook0_hatchery <- MARSS(subset_chinook_summer, model = mod.list2, method = "BFGS")
+
+
+
+### let's try individual years
+
+plot(data$doy[data$year == 2009 & data$doy > 120], 
+     data$chinook0_hatchery_num_interpolate[data$year == 2009  & data$doy > 120])
+
+plot(data$doy[data$year == 2009 & data$doy > 120], 
+     data$chinook0_wild_num[data$year == 2009  & data$doy > 120])
+
+#trying 2009 summer
+
+#subset
+subset_chinook_summer_2009 <- arrange(data,doy) %>%
+  filter(year == 2009 & doy > 120 & doy <= 209) %>%
+  mutate(log.value = ifelse(chinook0_wild_num == 0, NA, log(chinook0_wild_num))) %>%
+  select(log.value,year,doy) %>%
+  pivot_wider(names_from = year, values_from = log.value) %>%
+  column_to_rownames(var = "doy") %>%
+  as.matrix() %>%
+  t()
+
+
+#scale the values
+
+for(i in 1:dim(subset_chinook_summer_2009)[1]){
+  subset_chinook_summer_2009[i,] = scale(subset_chinook_summer_2009[i,])[,1]
+}
+
+
+covariates_all_years_summer_2009 <- arrange(data,doy) %>%
+  filter(year == 2009 & doy > 120 & doy <= 209) %>%
+  select(year,doy, temp, atu_april, photoperiod, flow, chinook0_hatchery_num_interpolate) %>%
+  pivot_wider(names_from = year, values_from = c(temp, atu_april, photoperiod, flow, chinook0_hatchery_num_interpolate)) %>%
+  column_to_rownames(var = "doy") %>%
+  as.matrix() %>%
+  t()
+
+
+#putting it into a loop
+mod.list <- list(
+  U = "zero",
+  R = "diagonal and equal",
+  Q = "diagonal and unequal",
+  C = matrix("c")
+)
+
+mod.list0 <- list(
+  U = "zero",
+  R = "diagonal and equal",
+  Q = "diagonal and unequal"
+)
+
+
+c.models <- list( 
+  c1 = "zero", # No covariates
+  c2 = zscore(covariates_all_years_summer_2009[1,]), # Temperature
+  c3 = zscore(covariates_all_years_summer_2009[2,]), # ATU April
+  c4 = zscore(covariates_all_years_summer_2009[3,]),  #Photoperiod
+  c5 = zscore(covariates_all_years_summer_2009[4,])  #Flow
+)
+names(c.models) <- c("No covariates", "Temperature", "ATU", "Photoperiod", "Flow")
+
+
+out.tab_2009 <- NULL
+fits_2009 <- list()
+for(i in 1:length(c.models)){
+  if(i == 1){
+    fit.model = c(list(c= c.models[[i]]), mod.list0)
+  }
+  else{
+    fit.model = c(list(c= c.models[[i]]), mod.list)
+  }
+  
+  fit_2009 <- MARSS(subset_chinook_summer_2009, model=fit.model, silent = TRUE,
+               control=list(maxit=2000, allow.degen=TRUE))
+  
+  
+  out=data.frame(c=names(c.models)[i], 
+                 logLik=fit$logLik, AICc=fit$AICc, num.param=fit$num.params,
+                 num.iter=fit$numIter, converged=!fit$convergence,
+                 stringsAsFactors = FALSE)
+  out.tab_2009=rbind(out.tab_2009,out)
+  fits_2009=c(fits,list(fit_2009))
+  
+}
+
+
+
+min.AICc <- order(out.tab_2009$AICc)
+out.tab.2009 <- out.tab_2009[min.AICc, ]
+out.tab.2009
+
+
+#loop not working - trying individually
+
+covariates_hatchery_2009 <- arrange(data,doy) %>%
+  filter(year == 2009 & doy > 120 & doy <= 209) %>%
+  select(year,doy,chinook0_hatchery_num_interpolate) %>%
+  pivot_wider(names_from = year, values_from = chinook0_hatchery_num_interpolate) %>%
+  column_to_rownames(var = "doy") %>%
+  as.matrix() %>%
+  t()
+
+
+
+mod.list_2009 <- list(
+  U = matrix("u"),
+  R = matrix("r"),
+  Q = matrix("q"),
+  C = matrix("c"),
+  c = covariates_hatchery_2009
+)
+
+
+fit_2009 <- MARSS(subset_chinook_summer_2009, model = mod.list_2009, method = "BFGS")
+fit_2009$AICc #178
+
+covariates_temp_2009 <- arrange(data,doy) %>%
+  filter(year == 2009 & doy > 120 & doy <= 209) %>%
+  select(year,doy,temp) %>%
+  pivot_wider(names_from = year, values_from = temp) %>%
+  column_to_rownames(var = "doy") %>%
+  as.matrix() %>%
+  t()
+
+mod.list_temp_2009 <- list(
+  U = "zero",
+  R = "diagonal and equal",
+  Q = "diagonal and unequal",
+  C = matrix("c"),
+  c = zscore(covariates_temp_2009)
+)
+
+
+fit_temp_2009 <- MARSS(subset_chinook_summer_2009, model = mod.list_temp_2009, method = "BFGS")
+fit_temp_2009$AICc #177
+
+
+mod.list0_2009 <- list(
+  U = "zero",
+  R = "diagonal and equal",
+  Q = "diagonal and unequal",
+  c = "zero"
+)
+
+
+fit0_2009 <- MARSS(subset_chinook_summer_2009, model = mod.list0_2009, method = "BFGS")
+fit0_2009$AICc #175
+
+
+
+#atu
+
+covariates_atu_2009 <- arrange(data,doy) %>%
+  filter(year == 2009 & doy > 120 & doy <= 209) %>%
+  select(year,doy,atu_april) %>%
+  pivot_wider(names_from = year, values_from = atu_april) %>%
+  column_to_rownames(var = "doy") %>%
+  as.matrix() %>%
+  t()
+
+mod.list_atu_2009 <- list(
+  U = "zero",
+  R = "diagonal and equal",
+  Q = "diagonal and unequal",
+  C = matrix("c"),
+  c = zscore(covariates_atu_2009)
+)
+
+
+fit_atu_2009 <- MARSS(subset_chinook_summer_2009, model = mod.list_atu_2009, method = "BFGS")
+fit_atu_2009$AICc #177
+
+
+#photoperiod
+
+
+covariates_photoperiod_2009 <- arrange(data,doy) %>%
+  filter(year == 2009 & doy > 120 & doy <= 209) %>%
+  select(year,doy,photoperiod) %>%
+  pivot_wider(names_from = year, values_from = photoperiod) %>%
+  column_to_rownames(var = "doy") %>%
+  as.matrix() %>%
+  t()
+
+mod.list_photoperiod_2009 <- list(
+  U = "zero",
+  R = "diagonal and equal",
+  Q = "diagonal and unequal",
+  C = matrix("c"),
+  c = zscore(covariates_photoperiod_2009)
+)
+
+
+fit_photo_2009 <- MARSS(subset_chinook_summer_2009, model = mod.list_photoperiod_2009, method = "BFGS")
+fit_photo_2009$AICc #178
+
+#flow
+
+covariates_flow_2009 <- arrange(data,doy) %>%
+  filter(year == 2009 & doy > 120 & doy <= 209) %>%
+  select(year,doy, flow) %>%
+  pivot_wider(names_from = year, values_from = flow) %>%
+  column_to_rownames(var = "doy") %>%
+  as.matrix() %>%
+  t()
+
+mod.list_flow_2009 <- list(
+  U = "zero",
+  R = "diagonal and equal",
+  Q = "diagonal and unequal",
+  C = matrix("c"),
+  c = zscore(covariates_flow_2009)
+)
+
+
+fit_flow_2009 <- MARSS(subset_chinook_summer_2009, model = mod.list_flow_2009, method = "BFGS")
+fit_flow_2009$AICc #177
+
+#trying flow and hatchery
+
+covariates_flow_hatchery_2009 <- arrange(data,doy) %>%
+  filter(year == 2009 & doy > 120 & doy <= 209) %>%
+  select(year,doy, flow, chinook0_hatchery_num_interpolate) %>%
+  pivot_wider(names_from = year, values_from = c(flow,chinook0_hatchery_num_interpolate)) %>%
+  column_to_rownames(var = "doy") %>%
+  as.matrix() %>%
+  t()
+
+
+mod.list_flow_hatchery_2009 <- list(
+  U = "zero",
+  R = "diagonal and equal",
+  Q = "diagonal and unequal",
+  C = matrix(list("c1", "c2")),
+  c = covariates_flow_hatchery_2009
+)
+
+fit_flow_hatchery_2009 <- MARSS(subset_chinook_summer_2009, model = mod.list_flow_hatchery_2009, method = "BFGS")
+fit_flow_hatchery_2009$AICc #178
+
