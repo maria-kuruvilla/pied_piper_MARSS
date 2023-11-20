6cef7ebaefc179517e9f5e18a2c929fc08d509da
maria-kuruvilla <maria@75799@gmail.com>
2023-11-20 10:14:34 -0800

    added combined data files and started analysis for chinook0

--- green/chinook0.R
+++ green/chinook0.R
@@ -30,6 +30,9 @@ data_day$temp_diff <- c(NA,diff(data_day$temp))
 data_day$flow_diff <- c(NA,diff(data_day$flow))
 data_day$photo_diff <- c(NA,diff(data_day$photoperiod))
 
+data_night$temp_diff <- c(NA,diff(data_night$temp))
+data_night$flow_diff <- c(NA,diff(data_night$flow))
+data_night$photo_diff <- c(NA,diff(data_night$photoperiod))
 
 lm_temp_day <- lm(temp ~ 1+photoperiod, data = data_day)
 
@@ -39,4 +42,31 @@ lm_temp_night <- lm(temp ~ 1+photoperiod, data = data_night)
 
 data_night <- data_night %>% add_residuals(lm_temp_night)
 
-#need to go back to the python code and add atu to the dataframe
+#need to go back to the python code and add atu to the dataframe - done
+
+
+data_day_night <- rbind(data_day,data_night)
+
+#plot chinook0_wild_perhour as a function of doy
+
+ggplot(data_day_night, aes(x = doy, y = chinook0_wild_perhour)) + geom_point() + facet_wrap(~daytime_category)
+
+#do same for chinook hatchery
+
+ggplot(data_day_night, aes(x = doy, y = chinook0_hatchery_perhour)) + geom_point() + facet_wrap(~daytime_category)
+
+#all the dates are messed up in data_night
+
+
+# covariates_chinook0 <- arrange(data_day_night,doy) %>%
+#   filter(doy >130 & doy <= 200) %>%
+#   dplyr::select(year,doy, daytime_category, temp, flow, photoperiod, atu_solstice, 
+#                 lunar_phase, resid, temp_diff, flow_diff, photo_diff, 
+#                 chinook0_hatchery_perhour_interpolate) %>%
+#   pivot_wider(names_from = c(year, daytime_category), values_from = c(
+#     temp, flow, photoperiod, atu_april, lunar_phase, 
+#     resid, temp_diff, flow_diff, photo_diff, chinook0_hatchery_perhour_interpolate)) %>%
+#   column_to_rownames(var = "doy") %>%
+#   as.matrix() %>%
+#   t()
+
